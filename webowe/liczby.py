56f3c6420a3f9a8e557f9827f1b221292fa8a41e
Mchl dm <jakisdziwnytenmail@gmail.com>
2025-10-15 01:58:58 +0200

    ZROBIONE

--- webowe/liczby.py
+++ webowe/liczby.py
@@ -14,7 +14,7 @@ def pobierz(plik):
     try:
         with open(plik, "+rt") as file:
             linie = file.readlines()
-            linie = [linia.replace("\n", "") for linia in linie]
+            linie = [int(linia.strip()) for linia in linie]
             return linie
             
     except FileNotFoundError:
@@ -29,7 +29,7 @@ def suma(liczby):
     suma = 0
     for liczba in liczby:
         suma += liczba
-        return suma
+    return suma
     
 def srednia(liczby):
     return suma(liczby) / len(liczby)
@@ -39,16 +39,35 @@ def minmax(liczby):
 
 def mediana(liczby):
     sort = sorted(liczby)
-    return sort[len(liczby) / 2]
+    n = len(sort)
+    if n % 2 == 1:
+        return sort[n // 2]
+    else:
+        return (sort[n // 2 - 1] + sort[n // 2]) / 2
 
+def wystapienia(liczby):
+    wystapienia = {}
+    for liczba in liczby:
+        if liczba in wystapienia:
+            wystapienia[liczba] += 1
+        else:
+            wystapienia[liczba] = 1
+    return wystapienia
 
 def main():
     plik = input("Podaj nazwę pliku")
-    generuj(plik)
+    #generuj(plik)
     liczby = pobierz(plik)
     
     print(f"Ilosc: {ilosc(liczby)}")
-    
+    print(f"Suma: {suma(liczby)}")
+    print(f"Srednia: {srednia(liczby)}")
+    print(f"Min i Max: {minmax(liczby)}")
+    print(f"Mediana: {mediana(liczby)}")
+    print(f"Wystapienia:")
+    wyst = wystapienia(liczby)
+    for liczba in sorted(wyst):
+        print(f"{liczba}: {wyst[liczba]}")
     
 
 
