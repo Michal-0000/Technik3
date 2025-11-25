29cdcbb9b5fbea61f1f8781a8245dc4d26dab9d0
Mchl dm <jakisdziwnytenmail@gmail.com>
2025-11-25 22:52:35 +0100

    dokonczone duplikaty cwiczenie7 pythony

--- webowe/cwiczenie7.py
+++ webowe/cwiczenie7.py
@@ -33,7 +33,6 @@ def zadanie2(linie):
                 print(f"({i+2}, {j+2})")
                 
 def zadanie3(linie):
-    najwieksza = 0
     najwiekszaIlosc = 0
     najwieksze = []
     for i in range(len(linie)):
@@ -45,28 +44,76 @@ def zadanie3(linie):
         if ilosc > najwiekszaIlosc:
             najwiekszaIlosc = ilosc
             najwieksze.clear()
-        else:
+        if ilosc == najwiekszaIlosc:
             najwieksze.append(linie[i])
-                
-    print(najwieksze)
+
+    najwiekszaCiag = ""
+    najwieksza = 0
+    for i in range(len(najwieksze)):
+        liczba = naTrojkowy(najwieksze[i])
+        if najwieksza < liczba:
+            najwiekszaCiag = najwieksze[i]
+            najwieksza = liczba
+            
+    print(najwieksza, najwiekszaCiag)
+
         
 def naTrojkowy(linia):
     suma = 0
     n = len(linia)
     for i in range(n):
-        if(linia[n - i] == '+'):
+        if(linia[n - i - 1] == '+'):
             suma += 3**i
-        elif(linia[n - i] == "*"):
+        elif(linia[n - i - 1] == "*"):
             suma += 2*3**i
     return suma
     
-            
+def zadanie4(linie):
+    suma = 0
+    for linia in linie:
+        suma += naTrojkowy(linia)
+    print(suma)
+    print(zDziesietnej(suma))
+
+
+def zDziesietnej(liczba):
+    wynik = ""
+    while liczba > 0:
+        reszta = liczba % 3
+        if reszta == 0:
+            wynik = "o" + wynik
+        elif reszta == 1:
+            wynik = "+" +  wynik
+        else:
+            wynik = "*" + wynik
+        liczba //= 3
+    return wynik
     
+def zadanie5(linie):
+    sety = set(linie)
+    if len(linie) == len(sety):
+        print("Nie powtarza sie")
+    else:
+        print("Powtarza sie")
+
+    widziane = set()
+    duplikaty = set()
+    for linia in linie:
+        if linia in widziane:
+            duplikaty.add(linia)
+        else:
+            widziane.add(linia)
+    
+    print(duplikaty)
+    
+
 def main():
     linie = wczytaj()
     zadanie1(linie)
     zadanie2(linie)
     zadanie3(linie)
+    zadanie4(linie)
+    zadanie5(linie)
 
 if __name__ == "__main__":
     main()
