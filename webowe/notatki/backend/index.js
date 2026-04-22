b26575d4a783b113cef764509cee5ada18754865
Szkola <szkola@gmail.com>
2026-04-22 09:31:48 +0200

    REST API notes

--- webowe/notatki/backend/index.js
+++ webowe/notatki/backend/index.js
@@ -16,3 +16,44 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage : storage})
+
+
+//endpoint - zapisywania notatki
+app.post('/upload', upload.single('image'), (req, res) => {
+    const {title, content} = req.body;
+    const imageURL = req.file ? `/assets/images/${req.file.filename}` : null;
+
+    db.run(`INSERT INTO notes (title, content, image) VALUES (?,?,?)`, [title, content, imageURL],
+        function(err){
+            if(err){
+            return res.status(500).json({error: err.message});
+            }
+            res.json({id: this.lastID, imageURL});
+        });
+});
+
+
+//endpoint - pobieranie wszystkich notatek z bazy
+app.get('/notes', (req, res) => {
+    db.all('SELECT * FROM notes', [], 
+        function(err, rows){
+            if(err){
+                return res.status(500).json({error: err.message});
+            }
+            res.json(rows);
+        });
+});
+
+//endpoint - usuwanie notatki z bazy
+app.delete(`/notes/:id`, (req, res) => {
+    const {id} = req.params;
+    db.run(`DELETE FROM notes WHERE id = ?`, [id],
+        function(err){
+            if(err){
+                return res.status(500).json({error: err.message});
+            }
+            res.json({message: "Usunieto rekord z bazy"});
+        });
+});
+
+app.listen(3000, () => console.log("Serwer dziala na porcie 3000..."));
