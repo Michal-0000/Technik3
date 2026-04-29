f248b4160d5d3c802540d79924914ccc4760503c
Szkola <szkola@gmail.com>
2026-04-29 13:57:30 +0200

    KONTROLA

---
+++ desktopowe/KontrolaSrodowiska/KontrolaSrodowiska/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KontrolaSrodowiska
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ desktopowe/KontrolaSrodowiska/KontrolaSrodowiska/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KontrolaSrodowiska
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async Task WilgotnoscAsync()
+        {
+            while (true)
+            {
+                int wilgotnosc = random.Next(20,81);
+                if( wilgotnosc < 30 ) { }
+
+                Dispatcher.Invoke(() =>
+                {
+                    txtWilgotnosc.Content = $"{wilgotnosc}%";
+                    if (wilgotnosc < 30) txtWilgotnosc;
+                });
+
+                await Task.Delay(800);
+            }
+        }
+
+        private async Task NawodnienieAsync()
+        {
+            while (true)
+            {
+
+
+                await Task.Delay(800);
+            }
+        }
+
+        private async Task LogiAsync()
+        {
+            while (true)
+            {
+
+
+                await Task.Delay(800);
+            }
+        }
+    }
+}
